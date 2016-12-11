7f80ade2bbc1236bfd900f49516aa0b16362cf8f
Daniel Wangai <mainad57@gmail.com>
2016-12-11 12:42:57 +0300

    Scheduler saves to db. Renames file after pushing its data.

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -4,21 +4,30 @@ require 'monitor_inv_directory'
 scheduler = Rufus::Scheduler.new
 
 directory = ENV['INV_DIRECTORY_PATH'] + "*.TXT"
-inventory = InventoryDirectory.get_all_files(directory)
+inventory_files = InventoryDirectory.get_all_files(directory)
 
-data = InventoryDirectory.split_txt_file(inventory[3])
-transation_items = InventoryDirectory.transation_items(data)
-transaction_hash = InventoryDirectory.transation_items_hash(transation_items)
+# directory containing inventory files
+Dir.chdir ENV['INV_DIRECTORY_PATH']
 
-scheduler.every '2s' do
+scheduler.every '5s' do
   # sheduler to save transaction details
-  transaction = Transaction.new
-  # save time
-  transaction.time = InventoryDirectory.get_date(data)[1]
-  transaction.item = InventoryDirectory.get_item(transaction_hash)
-  transaction.quantity = InventoryDirectory.get_quantity(transaction_hash)
-  transaction.cost = InventoryDirectory.get_cost(transaction_hash).to_f
-  transaction.total_cost = InventoryDirectory.get_total_cost(data).to_f
-  transaction.served_by = InventoryDirectory.get_served_by(data)
-  transaction.save!
+
+  inventory_files.each do |inventory|
+    data = InventoryDirectory.split_txt_file(inventory)
+    transation_items = InventoryDirectory.transation_items(data)
+    transaction_hash = InventoryDirectory.transation_items_hash(transation_items)
+    transaction = Transaction.new
+
+    transaction.time = InventoryDirectory.get_date(data)[1]
+    transaction.item = InventoryDirectory.get_item(transaction_hash)
+    transaction.quantity = InventoryDirectory.get_quantity(transaction_hash)
+    transaction.cost = InventoryDirectory.get_cost(transaction_hash).to_f
+    transaction.total_cost = InventoryDirectory.get_total_cost(data).to_f
+    transaction.served_by = InventoryDirectory.get_served_by(data)
+    transaction.save!
+
+    # rename current file
+    invoice_file = inventory.split("/").last
+    File.rename(invoice_file, File.join(Dir.pwd, "$"+invoice_file))
+  end
 end
