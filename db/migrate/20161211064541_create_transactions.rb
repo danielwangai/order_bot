0bb657c9b9958e77076beaa31dad5d8419af865f
Daniel Wangai <mainad57@gmail.com>
2016-12-11 10:04:57 +0300

    Added refactored transaction to contain precision.

--- db/migrate/20161211064541_create_transactions.rb
+++ db/migrate/20161211064541_create_transactions.rb
@@ -4,8 +4,8 @@ class CreateTransactions < ActiveRecord::Migration[5.0]
       t.string :time
       t.string :item
       t.integer :quantity
-      t.decimal :cost, :decimal, scale: 2
-      t.decimal :total_cost, :decimal, scale: 2
+      t.decimal :cost, :decimal, precision: 8, scale: 2
+      t.decimal :total_cost, :decimal, precision: 8, scale: 2
       t.string :served_by
 
       t.timestamps
